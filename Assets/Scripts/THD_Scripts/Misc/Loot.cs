f10b63b609a896a58bb6fc61058d460d12ceb242
Aliénor <alienor.tignon@gmail.com>
2019-10-15 17:43:34 +0200

    Rangement et rename

--- Assets/Scripts/THD_Scripts/Misc/Loot.cs
+++ Assets/Scripts/THD_Scripts/Misc/Loot.cs
@@ -6,7 +6,7 @@ public class Loot : MonoBehaviour
 {
     private bool canBePickedUp;
     private bool isCarrying;
-    private bool candrop
+    //private bool candrop
     Transform picker;
     // Start is called before the first frame update
     void Start()
