bf3bbe97285da484da021cc06a13c22da6717d83
Edouard Murat <e.murat@rubika-edu.com>
2019-11-05 21:46:25 +0100

    Imported sumobergine, citronin, o-gnon and frappasteque sprites

--- Assets/Scripts/Ennemies/Enemy.cs
+++ Assets/Scripts/Ennemies/Enemy.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    public float maximumHealth;
+    public float health;
+    public float knockbackResistance;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        health = maximumHealth;
+    }
+
     public void TakeDamage(float damage)
     {
-        Debug.Log("Hit " + gameObject.name);
+        
+    }
+
+    public void TakeKnockback()
+    {
+
     }
 }
