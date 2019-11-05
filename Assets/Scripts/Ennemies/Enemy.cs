4f567a910d2dbc2bc3bbb620a7c16580597d8026
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2019-11-05 15:10:35 +0100

    Début de la branche, modif enemy, onionbehaviour, ajout des spawners

--- Assets/Scripts/Ennemies/Enemy.cs
+++ Assets/Scripts/Ennemies/Enemy.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int          health;
+    public float        speed;
+    public float        attackDamage;
+    public bool         isAttacking;
+    private float       lootChance; //proba de drop init au spawn du mob
+    public float        lootPercentage; // proba de drop attaché à un type d'ennemi
+    public GameObject   loot;
+    public GameObject   target;
+
     public void TakeDamage(float damage)
     {
         Debug.Log("Hit " + gameObject.name);
+    }
+
+    public void Loot()
+    {
+        if (lootChance <= lootPercentage)
+        {
+            Instantiate(loot, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        }
+    }
+
+    public void OnDeath()
+    {
+        if (health <= 0)
+        {
+            // Death animation event
+            Loot();
+            Destroy(gameObject); //pour l'instant
+        }
+    }
+
+    private void Update()
+    {
+        OnDeath();
+    }
+
+    private void Start()
+    {
+        lootChance = Random.Range(0f, 1f);
     }
 }
---
+++ Assets/Scripts/Ennemies/Spawner/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject   enemyToSpawn;
+    float               spawnRate;
+    public float        ispawnRate;
+    public int          numberToSpawn;
+    public int          spawnUseCount;
+    public int          spawnUseCountMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnRate = ispawnRate;
+        spawnUseCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnUseCount < spawnUseCountMax)//Nombre de fois max que le spawner va s'activer
+        {
+            if (spawnRate <= 0)
+            {
+                for (int i = 0; i < numberToSpawn; i++) //Le spawner fait apparaitre x ennemis à la fois
+                {
+                    Instantiate(enemyToSpawn, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+                    spawnRate = ispawnRate;
+                }
+                spawnUseCount++;
+            }
+            else
+            {
+                spawnRate--;
+            }
+        }
+    }
+}
