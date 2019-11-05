d64745c06db8d77068151838a8f3f82da3420d56
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2019-11-05 16:50:32 +0100

    Fix Onion

--- Assets/Scripts/Ennemies/Onion.cs
+++ Assets/Scripts/Ennemies/Onion.cs
@@ -8,12 +8,13 @@ public class Onion : Enemy
     private Animator        anim;
     public float            stoppingDistance;
     public ParticleSystem   onionSpray;
-
+    public Transform pos;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        pos = target.transform;
     }
 
     // Update is called once per frame
@@ -23,6 +24,7 @@ public class Onion : Enemy
         OnDeathAttack();
         anim.SetFloat("horizontalMove", rb.velocity.x);
         anim.SetFloat("verticalMove", rb.velocity.y); 
+        
     }
 
 
@@ -30,10 +32,15 @@ public class Onion : Enemy
     {
         if (!isAttacking)
         {
-            if (Vector3.Distance(transform.position, target.transform.position) > stoppingDistance)
+            if (Vector2.Distance(transform.position, pos.position) > stoppingDistance)
             {
-                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
+                rb.velocity = (pos.position - transform.position).normalized * speed;
             }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+            
         }
 
         else
