ce7d02a81b72fe795d67407a057131abcd246863
Edouard MURAT <e.murat@rubika-edu.com>
2019-11-14 16:43:57 +0100

    Added onion and watermelon spawner

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    public enum State
+    {
+        MENU = 0,
+        HUB = 1,
+        RUN = 2
+    }
+    private State gameState = State.MENU;
     public List<Item> fridgeInventory;
 
     private void Awake()
@@ -15,5 +21,15 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         fridgeInventory = new List<Item>();
+    }
+
+    private void Update()
+    {
+        switch(gameState)
+        {
+            case State.MENU:
+
+                break;
+        }
     }
 }
