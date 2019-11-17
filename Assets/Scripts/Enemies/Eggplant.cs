479b4e7d5affd418c36a133342f1da315e527a87
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2019-11-17 15:41:42 +0100

    Début d'intégration de la sumobergine et de ses animations

---
+++ Assets/Scripts/Enemies/Eggplant.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eggplant : NewEnemy
+{
+    //public float t;
+    bool startShockwave;
+
+    //Movement 
+    Transform target;
+    public float stoppingDistance;
+
+    //Shockwave
+    public float shockwaveCooldown;
+    CircleCollider2D shockwave;
+    public float sizeDiffShock;   
+    public float speedShockWave;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnStart();
+        target = player.transform ;
+        shockwave = GetComponent<CircleCollider2D>();
+        attackCooldown = initialAttackCooldown;
+        startShockwave = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnUpdate();
+        EggplantAttack();
+
+        Movement();
+        Orientation();
+
+        anim.SetFloat("horizontalMovement", rb.velocity.x);
+        anim.SetFloat("verticalMovement", rb.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        if (startShockwave)
+        {
+            shockwave.radius += speedShockWave * Time.deltaTime;
+        }
+        else if (shockwave.radius >= stoppingDistance + sizeDiffShock)
+        {
+            startShockwave = false;
+            shockwave.radius = 0;
+        }
+    }
+
+    void Movement()
+    {   
+        if (Vector3.Distance(transform.position, target.position) >= stoppingDistance)
+        {
+            rb.velocity = (player.transform.position - transform.position).normalized * (baseSpeed * speedModifier);
+            anim.SetBool("isMoving", true);
+        }
+
+        else
+        {
+            rb.velocity = Vector2.zero;
+            anim.SetBool("isMoving", false);
+        }    
+    }
+
+    void EggplantAttack()
+    {
+
+            if (Vector3.Distance(target.position, transform.position) <= stoppingDistance)
+            {
+                rb.velocity = Vector2.zero;
+                if(shockwaveCooldown <= 0)
+                {
+                    anim.SetBool("isAttacking", true);
+                }
+                else
+                {
+                    shockwaveCooldown -= Time.deltaTime;
+                }
+
+            }  
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, shockwave.radius);
+    }
+
+    public void GetAnimationEvent(string eventMessage)
+    {
+        if (eventMessage.Equals("AttackEnded"))
+        {
+            startShockwave = true;
+            attackCooldown = initialAttackCooldown;
+            anim.SetBool("isAttacking", false);
+        }
+
+        if (eventMessage.Equals("Death"))
+        {
+            isDead = true;
+        }
+
+        if (eventMessage.Equals("Hit"))
+        {
+            anim.SetBool("isDamage", false);
+        }
+    }
+}
